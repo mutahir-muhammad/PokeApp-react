4d0e8e950d786253123662509b4c968fa5eb2ba5
Mutahir <mutahir.khan44@yahoo.com>
2023-07-21 09:03:28 +0500

    added API to fetch list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import PokeList from "./PokeList";
+import axios from "axios";
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
+  //using axios to fetch data from PokeAPI
+  useEffect(() => {
+    setLoading(true)
+    axios.get(currentPageUrl).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+  }, [currentPageUrl])
+
+  if(loading) return "Loading Pokemon..."
   return (
-    null
+   <PokeList pokemon = { pokemon }/>
   );
 }
 
